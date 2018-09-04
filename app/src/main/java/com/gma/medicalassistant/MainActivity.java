5c06dbdb455cba41eb7a9dfe7b668d4a67592b13
Lionel Shen <lionel.shen@autodesk.com>
2018-09-05 00:17:14 +0800

    Added login activity

--- app/src/main/java/com/gma/medicalassistant/MainActivity.java
+++ app/src/main/java/com/gma/medicalassistant/MainActivity.java
@@ -1,15 +1,21 @@
 package com.gma.medicalassistant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private int LOGIN_REQUEST_CODE = 10001;
+    private boolean mLogged = false;
+    private String TAG = "MainActivity";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,11 +26,14 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_activity:
+                    mTextMessage.setText(R.string.title_activity);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_signup:
+                    mTextMessage.setText(R.string.title_signup);
+                    return true;
+                case R.id.navigation_myprofile:
+                    mTextMessage.setText(R.string.title_myprofile);
                     return true;
             }
             return false;
@@ -41,4 +50,26 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mLogged) {
+            Intent loginIntent = new Intent("ACTION_LOGIN");
+            loginIntent.putExtra("key", "test");
+            int requestCode = LOGIN_REQUEST_CODE;
+            startActivityForResult(loginIntent, requestCode);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 10001) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getExtras().getString("result");
+                mLogged = result.equals("OK");
+                Log.i(TAG, result);
+            }
+        }
+    }
 }
